298909b9a3801df3bc58be652467fd5cbb4472c7
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-12 21:36:57 +0000

    Add Player vert. rotation and cursor lock

--- majproj-unity/Assets/Scripts/MechTest/MouseLook.cs
+++ majproj-unity/Assets/Scripts/MechTest/MouseLook.cs
@@ -7,11 +7,22 @@ public class MouseLook : MonoBehaviour
     [SerializeField] private float mouseSensitivity = 100f;
     [SerializeField] private Transform playerBody;
 
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     private void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
