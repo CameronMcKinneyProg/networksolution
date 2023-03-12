d021ecc460171aa9ab7d3836c3dc6620bd15c350
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-12 21:33:39 +0000

    Add Player horiz. rotation

--- majproj-unity/Assets/Scripts/MechTest/MouseLook.cs
+++ majproj-unity/Assets/Scripts/MechTest/MouseLook.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerBody;
 
     private void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
