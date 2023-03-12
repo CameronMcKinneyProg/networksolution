3df57634669aed42352bb58f8929c0b609de3eaf
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-12 22:06:56 +0000

    Add ground checking and ground layer mask

--- majproj-unity/Assets/Scripts/MechTest/PlayerMovement.cs
+++ majproj-unity/Assets/Scripts/MechTest/PlayerMovement.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
+
     [SerializeField] private float speed = 12f;
     [SerializeField] private float gravity = -9.81f;
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
     private Vector3 velocity;
+    private bool isGrounded;
 
     private void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // keeps player stuck to ground
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
